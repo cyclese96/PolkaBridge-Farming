d453cc76d0efdde85b805f09846b1fa6ca2ffbce
amirsiddiquiin <amirsiddiqui.in@gmail.com>
2021-09-22 21:30:56 +0530

    fix card ui

--- client/src/components/TopBar/components/Nav.tsx
+++ client/src/components/TopBar/components/Nav.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import DotCircle from './DotCircle'
-import { makeStyles } from "@material-ui/core/styles";
-import { white } from '../../../theme/colors';
+import { makeStyles } from '@material-ui/core/styles'
+import { white } from '../../../theme/colors'
+import Wallet from './Wallet'
 
 interface NavProp {
-    showMenu: boolean;
-
+  showMenu: boolean
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -15,13 +15,13 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   appBarBackground: {
-    boxShadow: "none",
-    backgroundColor: "#100525",
-    display: "flex",
-    alignItems: "center",
+    boxShadow: 'none',
+    backgroundColor: '#100525',
+    display: 'flex',
+    alignItems: 'center',
   },
   menuButton: {
-    textTransform: "none",
+    textTransform: 'none',
   },
   title: {
     fontWeight: 600,
@@ -35,33 +35,33 @@ const useStyles = makeStyles((theme) => ({
   sectionDesktop: {
     marginLeft: 40,
     marginRight: 40,
-    [theme.breakpoints.down("md")]: {
+    [theme.breakpoints.down('md')]: {
       marginLeft: 5,
       marginRight: 5,
     },
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
     },
   },
   sectionMobile: {
-    width: "100%",
-    display: "flex",
-    alignItems: "stretch",
-    justifyContent: "space-between",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
+    width: '100%',
+    display: 'flex',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
     },
   },
   row1: {
-    display: "flex",
-    width: "100%",
-    alignItems: "center",
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
   },
 
   home: {
-    "text-decoration": "none",
-    color: "black",
-    cursor: "pointer",
+    'text-decoration': 'none',
+    color: 'black',
+    cursor: 'pointer',
     marginRight: 5,
     marginLeft: 5,
   },
@@ -69,157 +69,178 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 15,
   },
   menuIcon: {
-    color: "#212121",
+    color: '#212121',
   },
 
   fullList: {
-    width: "auto",
+    width: 'auto',
   },
   menuTitleMobile: {
     paddingLeft: 25,
     fontWeight: 500,
-    verticalAlign: "baseline",
-    fontFamily: "New Rocker, cursive",
-    textAlign: "left",
+    verticalAlign: 'baseline',
+    fontFamily: 'New Rocker, cursive',
+    textAlign: 'left',
     fontSize: 16,
   },
   navbarItemsDesktop: {
     paddingRight: 10,
     fontWeight: 400,
-    lineHeight: "34px",
-    verticalAlign: "baseline",
-    letterSpacing: "-1px",
+    lineHeight: '34px',
+    verticalAlign: 'baseline',
+    letterSpacing: '-1px',
     margin: 0,
-    padding: "9px 14px 0px",
-    cursor: "pointer",
-    fontSize: "1.2vw",
-    color: "#e5e5e5",
-    textDecoration: "none",
+    padding: '9px 14px 0px',
+    cursor: 'pointer',
+    fontSize: '1.2vw',
+    color: '#e5e5e5',
+    textDecoration: 'none',
   },
   navbarItemsDesktopActive: {
     paddingRight: 10,
     fontWeight: 500,
-    lineHeight: "34px",
-    verticalAlign: "baseline",
-    letterSpacing: "-1px",
+    lineHeight: '34px',
+    verticalAlign: 'baseline',
+    letterSpacing: '-1px',
     margin: 0,
-    padding: "9px 14px 0px",
-    cursor: "pointer",
-    fontSize: "1.2vw",
-    color: "#e0077d",
-    textDecoration: "none",
-    
+    padding: '9px 14px 0px',
+    cursor: 'pointer',
+    fontSize: '1.2vw',
+    color: '#e0077d',
+    textDecoration: 'none',
   },
   navbarButton: {
-    backgroundColor: "#f9f9f9",
-    color: "#C80C81",
+    backgroundColor: '#f9f9f9',
+    color: '#C80C81',
     borderRadius: 10,
     height: 35,
     marginRight: 40,
     padding: 20,
     fontSize: 14,
     fontWeight: 700,
-    textTransform: "none",
-    "&:hover": {
-      background: "rgba(255, 255, 255, 0.7)",
+    textTransform: 'none',
+    '&:hover': {
+      background: 'rgba(255, 255, 255, 0.7)',
     },
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       marginRight: 0,
       marginLeft: 15,
       width: 150,
     },
   },
   mobileButton: {
-    borderRadius: "50px",
+    borderRadius: '50px',
     background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
-    lineHeight: "24px",
-    verticalAlign: "baseline",
-    letterSpacing: "-0.8px",
+    lineHeight: '24px',
+    verticalAlign: 'baseline',
+    letterSpacing: '-0.8px',
     margin: 0,
-    color: "#ffffff",
-    padding: "5px 15px 5px 15px",
+    color: '#ffffff',
+    padding: '5px 15px 5px 15px',
     fontWeight: 600,
   },
   leftMargin: {
     marginLeft: 159,
-    [theme.breakpoints.down("lg")]: {
+    [theme.breakpoints.down('lg')]: {
       marginLeft: 100,
     },
   },
   numbers: {
-    color: "#E0077D",
+    color: '#E0077D',
     fontSize: 26,
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       fontSize: 16,
     },
   },
   network: {
-    display: "flex",
+    display: 'flex',
     marginLeft: 20,
-    alignItems: "center",
-    border: "0.5px solid #919191",
+    alignItems: 'center',
+    border: '0.5px solid #919191',
     borderRadius: 20,
     padding: 4,
     paddingLeft: 6,
     paddingRight: 10,
     letterSpacing: 0.4,
     // cursor: "pointer",
-    "&:hover": {
-      background: "rgba(255, 255, 255, 0.1)",
+    '&:hover': {
+      background: 'rgba(255, 255, 255, 0.1)',
     },
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       width: 140,
     },
   },
   networkIcon: {
     width: 30,
-    height: "auto",
+    height: 'auto',
   },
   logo: {
     height: 38,
     width: 150,
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down('sm')]: {
       height: 30,
-      width: "fit-content",
+      width: 'fit-content',
     },
   },
   list: {
     paddingTop: 20,
-    width: "250px",
-    borderLeft: "5px solid pink",
-    borderColor: "#3A1242",
+    width: '250px',
+    borderLeft: '5px solid pink',
+    borderColor: '#3A1242',
     // borderColor: "#220c3d",
-    height: "100%",
-    backgroundColor: "#100525",
+    height: '100%',
+    backgroundColor: '#100525',
   },
-}));
+}))
 
-
-const Nav: React.FC<NavProp> = ({showMenu}) => {
-  const classes = useStyles();
+const Nav: React.FC<NavProp> = ({ showMenu }) => {
+  const classes = useStyles()
   return (
-    <StyledNav showMenu={showMenu} className={classes.sectionDesktop} >
-      <StyledAbsoluteLink href="https://stake.polkabridge.org" className={classes.navbarItemsDesktop}>
-        Stake <DotCircle/>
-      </StyledAbsoluteLink> 
+    <StyledNav showMenu={showMenu} className={classes.sectionDesktop}>
+      <StyledAbsoluteLink
+        href="https://stake.polkabridge.org"
+        className={classes.navbarItemsDesktop}
+      >
+        Stake <DotCircle />
+      </StyledAbsoluteLink>
       <StyledAbsoluteLink href="/" className={classes.navbarItemsDesktopActive}>
-        Farm<DotCircle/>
+        Farm
+        <DotCircle />
       </StyledAbsoluteLink>
-      
-      <StyledAbsoluteLink href="https://launchpad.polkabridge.org" className={classes.navbarItemsDesktop}>
-        Launchpad<DotCircle/>
+      <StyledAbsoluteLink
+        href="https://launchpad.polkabridge.org"
+        className={classes.navbarItemsDesktop}
+      >
+        Launchpad
+        <DotCircle />
       </StyledAbsoluteLink>
-      <StyledAbsoluteLink href="https://swap.polkabridge.org" className={classes.navbarItemsDesktop}>
-        Swap<DotCircle/>
+      <StyledAbsoluteLink
+        href="https://swap.polkabridge.org"
+        className={classes.navbarItemsDesktop}
+      >
+        Swap
+        <DotCircle />
       </StyledAbsoluteLink>
-      <StyledAbsoluteLink href="https://lending.polkabridge.org" className={classes.navbarItemsDesktop}>
-        Lending<DotCircle/>
+      <StyledAbsoluteLink
+        href="https://lending.polkabridge.org"
+        className={classes.navbarItemsDesktop}
+      >
+        Lending
+        <DotCircle />
       </StyledAbsoluteLink>
-      <StyledAbsoluteLink href="https://prediction.polkabridge.org" className={classes.navbarItemsDesktop}>
-        Prediction<DotCircle/>
+      <StyledAbsoluteLink
+        href="https://prediction.polkabridge.org"
+        className={classes.navbarItemsDesktop}
+      >
+        Prediction
+        <DotCircle />
       </StyledAbsoluteLink>
-      <StyledAbsoluteLink href="https://corgib.polkabridge.org/bet"className={classes.navbarItemsDesktop}>
-        Betting<DotCircle/>
+      <StyledAbsoluteLink
+        href="https://corgib.polkabridge.org/bet"
+        className={classes.navbarItemsDesktop}
+      >
+        Betting
+        <DotCircle />
       </StyledAbsoluteLink>
     </StyledNav>
   )
--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -10,99 +10,299 @@ import CustomCountDown from './components/CustomCountDown'
 import { START_REWARD_AT_BLOCK } from '../../pbr/lib/constants'
 import PolkaBridgeLogo from '../../assets/img/logo-icon.svg'
 import FarmCards from '../Farms/components/FarmCards'
-import TotalValueLocked from './components/TotalValueLocked';
-import TotalClaimReward from './components/TotalClaimReward';
-import TokenPrice from './components/TokenPrice';
+import TotalValueLocked from './components/TotalValueLocked'
+import TotalClaimReward from './components/TotalClaimReward'
+import TokenPrice from './components/TokenPrice'
+import { Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme) => ({
+  earn: {
+    textAlign: 'center',
+    color: '#f9f9f9',
+    fontSize: 12,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
+  desktop: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+  tokenAmount: {
+    fontWeight: 700,
+    padding: 0,
+    paddingLeft: 10,
+    fontSize: 18,
+    color: '#C80C81',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tokenTitle: {
+    fontWeight: 500,
+    padding: 0,
+    paddingLeft: 10,
+    fontSize: 14,
+    paddingBottom: 3,
+    color: '#e5e5e5',
+  },
+  tokenSubtitle: {
+    fontWeight: 300,
+    padding: 0,
+    paddingLeft: 10,
+    fontSize: 12,
+    color: '#bdbdbd',
+  },
+  card: {
+    minHeight: 300,
+    width: '100%',
+
+    borderRadius: 30,
+    backgroundColor: 'rgba(41, 42, 66, 0.3)',
+    border: '1px solid #212121',
+    filter: 'drop-shadow(0 0 0.5rem #212121)',
+    [theme.breakpoints.down('sm')]: {
+      minWidth: 240,
+      width: '100%',
+    },
+  },
+  logo: {
+    width: 70,
+    height: 70,
+    marginTop: 5,
+    marginBottom: 10,
+    backgroundColor: 'transparent',
+    border: '1px solid #f9f9f9',
+    padding: 12,
+    borderRadius: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: 50,
+      height: 50,
+      marginBottom: 10,
+    },
+  },
+  buynow: {
+    background: `linear-gradient(to bottom,#D9047C, #BF1088)`,
+    color: 'white',
+    width: 100,
+    height: 40,
+    textTransform: 'none',
+    fontSize: 15,
+    borderRadius: 40,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    '&:hover': {
+      background: 'rgba(224, 7, 125, 0.7)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: 120,
+      fontSize: 15,
+    },
+  },
+  background: {
+    paddingTop: 50,
+    paddingLeft: 80,
+    paddingRight: 80,
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: 0,
+      paddingRight: 0,
+    },
+  },
+  divider: {
+    width: 90,
+    height: 3,
+    background: 'linear-gradient(to right, #e0077d, rgba(0, 0, 0, 0.4))',
+  },
+  butt: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+}))
 
 const Home: React.FC = () => {
   var block = 99999999999
   const launchBlock = START_REWARD_AT_BLOCK
   const [atDate, setDate] = useState<any>()
-
+  const classes = useStyles()
   return (
     <Page>
-        <SpacerRes>
-            <Spacer size="lg" />
-            <StyledLogo>
-                <img className="d-md-none" src={PolkaBridgeLogo} height="120" style={{ marginTop: -4 }} />
-            </StyledLogo>
-        </SpacerRes>
-        <Spacer size="lg" />
-        <div style={{fontWeight: 'bold', fontSize: 22, color: '#ffffff', textAlign: 'center'}}>
-            PBR Price: <span style={{color: '#e0077d', fontSize: 30}}>$<TokenPrice /></span>
+      <div className="container">
+        <div className={classes.background}>
+          <h1 className={classes.title}>Farm Pools</h1>
+          <div className={classes.divider} />
+          <div className="row mt-5">
+            <div className="col-md-8 mb-3">
+              <div className={classes.card}>
+                <div>
+                  <div className={classes.butt}>
+                    <Avatar
+                      src={'https://stake.polkabridge.org/img/symbol.png'}
+                      className={classes.logo}
+                    />
+                  </div>
+                  <div className={classes.butt}>
+                    <h6 className={classes.title}>PBR Statistics</h6>
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <div
+                      style={{
+                        backgroundColor: '#C80C81',
+                        borderRadius: '50%',
+                        height: '5px',
+                        width: '5px',
+                        marginRight: 5,
+                      }}
+                    >
+                      .
+                    </div>
+                    <div className={classes.earn}>
+                      All in one defi application
+                    </div>
+                  </div>
+                  <div className={classes.butt}>
+                    <a
+                      target="_blank"
+                      href="https://app.uniswap.org/#/swap?inputCurrency=0x298d492e8c1d909d3f63bc4a36c66c64acb3d695&outputCurrency=ETH&use=V2"
+                    >
+                      <div className={classes.buynow}> Buy Now</div>
+                    </a>
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <div className={classes.desktop}>
+                      <div className="text-center mt-4">
+                        <div className={classes.tokenTitle}>PBR Price: </div>
+                        <div className={classes.tokenAmount}>
+                          $<TokenPrice />
+                        </div>
+                      </div>
+                      <div className="text-center mt-4">
+                        <div className={classes.tokenTitle}>
+                          Total Value Locked (TVL):{' '}
+                        </div>
+                        <div className={classes.tokenAmount}>
+                          {' '}
+                          $<TotalValueLocked />
+                        </div>
+                      </div>
+                      <div className="text-center mt-4">
+                        <div className={classes.tokenTitle}>
+                          Total Claimed Rewards:{' '}
+                        </div>
+                        <div className={classes.tokenAmount}>
+                          <TotalClaimReward />
+                          PBR
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div style={{fontWeight: 'bold', fontSize: 22, color: '#ffffff', textAlign: 'center'}}>
-            Total Value Locked (TVL): <span style={{color: '#e0077d', fontSize: 30}}>$<TotalValueLocked /></span>
-        </div>
+      </div>
 
-        <div style={{fontWeight: 'bold', fontSize: 22, color: '#ffffff', textAlign: 'center'}}>
-            Total Claimed Rewards: <span style={{color: '#e0077d', fontSize: 30}}><TotalClaimReward /> PBR</span>
-        </div>
-
-        {block < launchBlock && atDate && <>
-            <Spacer size="sm" />
-            <CustomCountDown date={atDate}/>
-            <Spacer size="md" />
-            <div>
-                <ReadMore href="https://t.me/polkabridge" target="__blank"> Read The Announcement</ReadMore>
-                <ReadMore href="https://youtu.be/xBnpTe1qdAc?t=169" target="__blank"> Video Tutorial</ReadMore>
-                <div style={{color: 'rgb(255,255,255,0.6)', textAlign: 'center', marginTop: 5}}>Contract was fully audited by CertiK. Check report <a href="https://certik.org/projects/polkabridge" target="_blank">here</a>.</div>
-             
+      {block < launchBlock && atDate && (
+        <>
+          <Spacer size="sm" />
+          <CustomCountDown date={atDate} />
+          <Spacer size="md" />
+          <div>
+            <ReadMore href="https://t.me/polkabridge" target="__blank">
+              {' '}
+              Read The Announcement
+            </ReadMore>
+            <ReadMore
+              href="https://youtu.be/xBnpTe1qdAc?t=169"
+              target="__blank"
+            >
+              {' '}
+              Video Tutorial
+            </ReadMore>
+            <div
+              style={{
+                color: 'rgb(255,255,255,0.6)',
+                textAlign: 'center',
+                marginTop: 5,
+              }}
+            >
+              Contract was fully audited by CertiK. Check report{' '}
+              <a href="https://certik.org/projects/polkabridge" target="_blank">
+                here
+              </a>
+              .
             </div>
-            <Spacer size="lg" />
-            </>
-        }
-        {block >= launchBlock && <>
-            <Spacer size="lg" />
-            <Container>
-                <Balances />
-            </Container>
-            <Spacer size="md" />
-            <div style={{textAlign: 'center'}}>
-                <ReadMore href="https://t.me/polkabridge" target="__blank"> Read The Announcement </ReadMore>
-                <ReadMore href="https://youtu.be/xBnpTe1qdAc?t=169" target="__blank"> Video Tutorial</ReadMore>
-                <div style={{color: 'rgb(255,255,255,0.6)', textAlign: 'center', marginTop: 5}}>Contract was fully audited by CertiK. Check report <a href="https://certik.org/projects/polkabridge" target="_blank">here</a>.</div>
-              
+          </div>
+          <Spacer size="lg" />
+        </>
+      )}
+      {block >= launchBlock && (
+        <>
+          <Spacer size="lg" />
+          <Container>
+            <Balances />
+          </Container>
+          <Spacer size="md" />
+          <div style={{ textAlign: 'center' }}>
+            <ReadMore href="https://t.me/polkabridge" target="__blank">
+              {' '}
+              Read The Announcement{' '}
+            </ReadMore>
+            <ReadMore
+              href="https://youtu.be/xBnpTe1qdAc?t=169"
+              target="__blank"
+            >
+              {' '}
+              Video Tutorial
+            </ReadMore>
+            <div
+              style={{
+                color: 'rgb(255,255,255,0.6)',
+                textAlign: 'center',
+                marginTop: 5,
+              }}
+            >
+              Contract was fully audited by CertiK. Check report{' '}
+              <a href="https://certik.org/projects/polkabridge" target="_blank">
+                here
+              </a>
+              .
             </div>
-            {/*<Spacer size="lg" />*/}
-            </>
-        }
+          </div>
+          {/*<Spacer size="lg" />*/}
+        </>
+      )}
+      <Spacer size="lg" />
+      <Container size="lg">
+        <div
+          style={{
+            border: '1px solid #2C3030',
+          }}
+        ></div>
+      </Container>
+      <Box className="mt-4">
+        <StyledHeading>SELECT YOUR FIELDS</StyledHeading>
+        <StyledParagraph>Earn PBR tokens by staking LP token</StyledParagraph>
         <Spacer size="lg" />
-        <Container size = "lg">
-            <div style={{
-                border: '1px solid #2C3030'
-                }}>
-            </div>
-        </Container>
-        <Box className="mt-4">
-            <StyledHeading>SELECT YOUR FIELDS</StyledHeading>
-            <StyledParagraph>Earn PBR tokens by staking LP token</StyledParagraph>
-            <Spacer size="lg" />
-            <FarmCards />
-        </Box>
+        <FarmCards />
+      </Box>
     </Page>
   )
 }
 
-const StyledInfo = styled.h3`
-  color: ${(props) => props.theme.color.white};
-  font-size: 16px;
-  font-weight: 400;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-  display: flex;
-  align-items: start;
-  justify-content: center;
-  > img{
-    width: 20px;
-    margin-right: 3px;
-  }
-  b {
-    color: ${(props) => props.theme.color.primary.main};
-  }
-`
 const StyledHeading = styled.h2`
   color: ${(props) => props.theme.color.white};
   text-transform: uppercase;
@@ -130,36 +330,36 @@ const ReadMore = styled.a`
 `
 
 const StyledLogo = styled.div`
-    .d-md-none {
-        @media (max-width: 1024px) {
-            display: none;
-        }
+  .d-md-none {
+    @media (max-width: 1024px) {
+      display: none;
     }
-    .d-lg-none {
-        @media (min-width: 1025px) {
-            display: none;
-        }
+  }
+  .d-lg-none {
+    @media (min-width: 1025px) {
+      display: none;
     }
+  }
 `
 
 const Box = styled.div`
-    &.mt-4 {
-        margin-top: 40px;
-        @media (max-width: 767px) {
-            margin-top: 30px;
-        }
+  &.mt-4 {
+    margin-top: 40px;
+    @media (max-width: 767px) {
+      margin-top: 30px;
     }
+  }
 `
 const SpacerRes = styled.div`
-    .sc-iCoHVE {
-        @media (max-width: 1024px) {
-            display: none;
-        }
+  .sc-iCoHVE {
+    @media (max-width: 1024px) {
+      display: none;
     }
-    .d-lg-none {
-        @media (min-width: 1025px) {
-            display: none;
-        }
+  }
+  .d-lg-none {
+    @media (min-width: 1025px) {
+      display: none;
     }
+  }
 `
 export default Home
